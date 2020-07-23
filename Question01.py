870035711798cfc45ea3c104220f6b1e4b9884fe
Jay_BetaScientist <fh1328@wayne.edu>
2020-07-23 18:41:13 -0400

    Defined more methods. Updated notes.

--- Question01.py
+++ Question01.py
@@ -2,6 +2,8 @@
 # Assignment: 5
 # Question: 1
 
+import math
+
 
 class Algorithm:
     def __init__(self):
@@ -19,19 +21,71 @@ class Algorithm:
 
 class PopulationSpace:
     """Evolutionary Search. Candidate Solutions."""
-    population = None
-    time = None
 
     def __init__(self):
+        self.population = []
+        self.elites = []
+        self.pop_size = 0
         pass
 
-    def update(self):
+    def _generate(self):
+        """Generates the initial 50 random solutions (i.e. the population).
+        Values range from -10 to 110."""
+        pass
+
+    def evaluate(self):
+        """Evaluates the performance of the Population's individuals.
+        Method defined as "obj()" in academic paper."""
+        for solValue in self.population:
+            if solValue > 100:
+                solValue = self._larger_than_100(solValue)
+            else:
+                solValue = self._less_than_100(solValue)
+
+    @staticmethod
+    def _larger_than_100(value):
+        """Piecewise 1 of 2
+        x = -exp(-1) + (x - 100)(x - 102)
+        """
+        product = (value - 100) * (value - 102)
+        return -(math.exp(-1)) + product
+
+    @staticmethod
+    def _less_than_100(value):
+        """Piecewise 2 of 2
+        x = -exp(-(x / 100)^2))
+        """
+        
+
+    def _rank(self):
+        """Sorts the Population by their fitness score.
+        Fitness: piecewise function as defined by assignment instructions.
+        """
+        self.population.sort()
+
+    def _filter(self):
+        """Selects the top 20% of scores (i.e. the elites)."""
+        # CHECK: will error occur when population is <2
+        low_bound = len(self.population) * 0.2
+        low_bound = math.ceil(low_bound)
+        self.elites = self.population[low_bound:-1]
+
+    def accept(self):
+        """Determines the individuals of the Population that will influence
+        the Belief Space."""
+        self._rank()
+        self._filter()
+
+    def influence(self):
+        """Selects the next generation's individuals using knowledge (influence)
+         from the Belief Space."""
         pass
 
 
 class BeliefSpace:
-    """Knowledge. Information stored by ancestors, accessed by current generation."""
+    """Information of ancestors (i.e knowledge), accessed by current/future generations."""
     range = None
+
     # Accepted Range (Normative Knowledge)
     #   if (situational > lower bound) && (situational < upper bound)
 
@@ -41,34 +95,29 @@ class BeliefSpace:
     def __init__(self):
         pass
 
-    @staticmethod
-    def elevate_to_good_range():
+    def update(self):
+        """Adds the experiences of the accepted individuals of the Population."""
+        # popObj.accept()
+        #
+        # for individual in List population
+        #   if (not acceptable, pop(counter))
+        #       counter = counter + 1
         pass
 
 
-def evaluate(population):
+def check_termination_condition():
+    """Controls program termination. In this case, when time=100. [UPDATE]"""
+    # [?] if population == 1
     pass
-
-
-def update(beliefObj, popObj):
-    pass
-    # popObj.accept()
-    #
-    # for individual in List population
-    #   if (not acceptable, pop(counter))
-    #       counter = counter + 1
-
-
-def generate(beliefObj, popObj):
-    pass
-    # beliefObj.influence()
 
 
 if __name__ == '__main__':
-    time = 0
+    TIME = 0
 
+    population = PopulationSpace()
+    population.obj()
 
-# PseudoCode from Instructions:
+# --| PseudoCode for Each Generation |--
 #   Begin
 #       t = 0
 #       initialize Bt, Pt
@@ -81,18 +130,8 @@ if __name__ == '__main__':
 #           until(termination condition achieved)
 #   End
 
-# --| Functions |--
-#   - When an individual is to be modified: select ONE knowledge source to modify
-#       the individual. Selection is done by
-#   - Functions:
-#       accept() - selects the individual experiences that will be used to update
-#                       the belief space at each generation.
-#       update() - method in which knowledge sources communicate with each other
-#       influence() - determines how knowledge can influence the population
-#       select() - done via roulette wheel selection
-#   - Example for handling infeasible solutions:
-#       if (individual satisfies all constrains)
-#         fitness = obj(individual)
-#       else
-#         fitness = IN_FEASIBLE_VALUE
-#       endif
+# def evaluate_router(self):
+#     """Mimics a piecewise by routing to static method based on value passed-in.
+#     Evaluates the performance of the Population's individuals.
+#     Performance is a piecewise function as defined by assignment instructions."""
+#     pass
