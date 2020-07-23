315b37936051b439bcf2c6afc4f01b4b43cbc5c7
JHuraibi <fh1328@wayne.edu>
2020-07-23 12:34:10 -0400

    (Additional Notes and Edits to initial skeleton)

--- Question01.py
+++ Question01.py
@@ -3,7 +3,21 @@
 # Question: 1
 
 
-class Population:
+class Algorithm:
+    def __init__(self):
+        pass
+
+    def select(self):
+        pass
+
+    def accept(self):
+        pass
+
+    def influence(self):
+        pass
+
+
+class PopulationSpace:
     """Evolutionary Search. Candidate Solutions."""
     population = None
     time = None
@@ -11,13 +25,18 @@ class Population:
     def __init__(self):
         pass
 
-    def update(self, ):
+    def update(self):
         pass
 
 
-class Belief:
+class BeliefSpace:
     """Knowledge. Information stored by ancestors, accessed by current generation."""
     range = None
+    # Accepted Range (Normative Knowledge)
+    #   if (situational > lower bound) && (situational < upper bound)
+
+    # Exemplar Value (Situational Knowledge)
+    #   if (actual - situational) >= [tbd value]
 
     def __init__(self):
         pass
@@ -27,8 +46,27 @@ class Belief:
         pass
 
 
-if __name__ == '__main__':
+def evaluate(population):
     pass
+
+
+def update(beliefObj, popObj):
+    pass
+    # popObj.accept()
+    #
+    # for individual in List population
+    #   if (not acceptable, pop(counter))
+    #       counter = counter + 1
+
+
+def generate(beliefObj, popObj):
+    pass
+    # beliefObj.influence()
+
+
+if __name__ == '__main__':
+    time = 0
+
 
 # PseudoCode from Instructions:
 #   Begin
@@ -43,3 +81,18 @@ if __name__ == '__main__':
 #           until(termination condition achieved)
 #   End
 
+# --| Functions |--
+#   - When an individual is to be modified: select ONE knowledge source to modify
+#       the individual. Selection is done by
+#   - Functions:
+#       accept() - selects the individual experiences that will be used to update
+#                       the belief space at each generation.
+#       update() - method in which knowledge sources communicate with each other
+#       influence() - determines how knowledge can influence the population
+#       select() - done via roulette wheel selection
+#   - Example for handling infeasible solutions:
+#       if (individual satisfies all constrains)
+#         fitness = obj(individual)
+#       else
+#         fitness = IN_FEASIBLE_VALUE
+#       endif
