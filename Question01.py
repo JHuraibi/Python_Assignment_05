c999e719fc5c8c489878442f2c0f0295fbc77032
JHuraibi <fh1328@wayne.edu>
2020-07-24 13:21:17 -0400

    Update Question01.py

--- Question01.py
+++ Question01.py
@@ -10,7 +10,7 @@ class PopulationSpace:
     """Evolutionary Search. Candidate Solutions."""
 
     def __init__(self):
-        self.population = []
+        self.current_gen = []
         self.elites = []
         self.pop_size = 0
         self.range_lower_bound = -10
@@ -25,7 +25,7 @@ class PopulationSpace:
     def evaluate(self):
         """Evaluates the performance of the Population's individuals.
         Method defined as "obj()" in academic paper."""
-        for solution in self.population:
+        for solution in self.current_gen:
             if solution > 100:
                 solution = self._larger_than_100(solution)
             else:
@@ -49,18 +49,18 @@ class PopulationSpace:
         return -(math.exp(inner))
 
     def _rank(self):
+        # CRITICAL: Elites and super elite are not correctly being determined
         """Sorts the Population by their fitness score (DESCENDING!)
         Fitness: piecewise function as defined by assignment instructions.
         Sorting is reversed so that indexes can also intuitively associate a Solution's fitness.
         e.g. index 0 is always the "Super Elite" and 0 to [(size * 0.2)] are the Elites.
         """
-        self.population.sort(reverse=True)
+        self.current_gen.sort(reverse=True)
 
     def accept(self):
         """Determines the individuals of the Population that will influence
         the Belief Space."""
         self._rank()
-        self._filter()
 
     def _normative_knowledge(self, value):
         """Checks if the value is a new min or max"""
@@ -81,54 +81,59 @@ class BeliefSpace:
         self.elites = []
         self.super_elite = -10
 
-    def update(self, current_generation):
+    def update(self, current_gen):
         """Adds the experiences of the accepted individuals of the Population by:
         1. Recording the current generation's top performers (elites).
         2. Recording the current generation's minimum and maximum values."""
         # CHECK: will error occur when population is <2 ?
 
-    def _record_elites(self, current_generation):
-        low_bound = len(current_generation) * 0.2
+    def _record_elites(self, current_gen):
+        low_bound = len(current_gen) * 0.2
         low_bound = math.ceil(low_bound)
-        self.elites = current_generation[0:low_bound]
-        self.super_elite = current_generation[0]
+        self.elites = current_gen[0:low_bound]
+        self.super_elite = current_gen[0]
 
-    def _record_extrema(self, current_generation):
-        self.minima = current_generation[-1]
-        self.maxima = current_generation[0]
+    def _record_extrema(self, current_gen):
+        self.minima = current_gen[-1]
+        self.maxima = current_gen[0]
 
-    def influence(self, current_generation):
-        """Selects the next generation's individuals using knowledge (influence)
-         from the Belief Space.
-         i.e. modifies each individual in the """
+    def influence(self, current_gen):
+        """Generates the next generation's individuals using knowledge from the Belief Space."""
         # TODO: Move to Population class?
-        next_generation = current_generation                                    # Redundant, but helps intuitive reading
-        for solution in next_generation:
+        next_gen = []                                                           # Redundant, but helps intuitive reading
+        local_min = self.minima
+        local_max = self.maxima
+
+        for i in range(0, len(current_gen)):
+            individual = current_gen[i]
             mutation_occurs = random.randint(0, 1)
             if mutation_occurs:
-                solution = random.randint(-10, 100)
-            elif solution > self.super_elite:
-                solution = solution - 1
-            elif solution < self.super_elite:
-                solution = solution + 1
+                next_gen[i] = random.randint(local_min, local_max)
+            elif individual > self.super_elite:
+                next_gen[i] = individual - 1
+            elif individual < self.super_elite:
+                next_gen[i] = individual + 1
+            else:
+                next_gen[i] = current_gen[i]
 
-        return next_generation
-
-
-
-def check_termination_condition():
-    """Controls program termination. In this case, when time=100. [UPDATE]"""
-    # [?] if population == 1
-    pass
+        return next_gen
 
 
 if __name__ == '__main__':
-    TIME = 0
+    time = 0
+    endTime = 100
 
     population = PopulationSpace()
-    population.obj()
+    belief = BeliefSpace()
 
-    belief = BeliefSpace(x)
+    while time < endTime:
+        print("[Time: {}]\n".format(time))
+        population.evaluate()
+        belief.update(population.current_gen)
+        time = time - 1
+
+    print("\ndone\n")
+
 
 # --| PseudoCode for Each Generation |--
 #   Begin
@@ -136,11 +141,11 @@ if __name__ == '__main__':
 #       initialize Bt, Pt
 #       repeat
 #           evaluate Pt
-#           update(Bt, accept(Pt))
-#           generate(Pt, influence(Bt))
-#           t = t + 1;
-#           select Pt from Pt - 1
-#           until(termination condition achieved)
+#           update(Bt, accept(Pt))                  # ?staticmethod
+#           generate(Pt, influence(Bt))             # ?staticmethod
+#           t = t + 1;                              # update time
+#           select Pt from Pt - 1                   # ?remove 1 solution
+#           until(termination condition achieved)   # check loop condition
 #   End
 
 # def evaluate_router(self):
