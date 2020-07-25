a33a3ff5413ce49a534200882eeae3d0793e7dca
JHuraibi <fh1328@wayne.edu>
2020-07-25 16:42:24 -0400

    Update TestingCode.py

--- TestingCode.py
+++ TestingCode.py
@@ -2,5 +2,19 @@
 # Assignment []
 # Question []
 
-if __name__ == '__main__'
+from decimal import *
 
+if __name__ == '__main__':
+
+    value = 100 + Decimal(1e-20)
+    test = 100.00000000000000000004
+
+    decTest = Decimal(test)
+
+    if test > (100 + Decimal(1e-20)):
+        print("TRUE")
+
+    if decTest > (100 + Decimal(1e-20)):
+        print("TRUE")
+
+    # print(value)
